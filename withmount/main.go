4b20cecd022edc2c8442e2306cacc5ca882e2b79
Nitin Nizhawan <nitin.nizhawan@gmail.com>
2018-06-11 18:58:07 +0530

    Improved with mount

--- withmount/main.go
+++ withmount/main.go
@@ -12,7 +12,7 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		parent()
-	case "child":
+	case "init":
 		child()
 	default:
 		panic("wat should I do")
@@ -24,9 +24,9 @@ func parent() {
 	fmt.Println(os.Args[0])
 	fmt.Println(os.Args[1])
 	fmt.Println(os.Args[2])
-	cmd := exec.Command(os.Args[0], append([]string{"child"}, os.Args[2:]...)...)
+	cmd := exec.Command(os.Args[0], append([]string{"init"}, os.Args[2:]...)...)
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Cloneflags: syscall.CLONE_NEWNS,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
@@ -43,11 +43,11 @@ func child() {
 	tempDir,_ := ioutil.TempDir("","dock");
 	fmt.Println("TempDir :"+tempDir);
 	syscall.Mount(os.Args[2],tempDir,"",syscall.MS_BIND | syscall.MS_PRIVATE,"");
-	must(os.MkdirAll(tempDir+"/proc", 0700))
-	must(syscall.Mount("/proc",tempDir+"/proc","proc",syscall.MS_BIND | syscall.MS_PRIVATE,""));
 	must(os.MkdirAll(tempDir+"/oldrootfs", 0700))
 	must(syscall.PivotRoot(tempDir, tempDir+"/oldrootfs"))
 	must(os.Chdir("/"))
+	must(os.MkdirAll("/proc", 0700))
+	must(syscall.Mount("proc","/proc","proc",0,""));
         syscall.Unmount("oldrootfs",syscall.MNT_DETACH)
 	os.RemoveAll("oldrootfs")
 	cmd := exec.Command(os.Args[3], os.Args[4:]...)
